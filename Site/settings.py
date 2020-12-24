2c978daefc14a7d2a964afd864b34458df2c541f
krabaton <krabatua@gmail.com>
2020-12-24 14:32:08 +0200

    'Prepare'

--- Site/settings.py
+++ Site/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,27 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "blog/static",
-    BASE_DIR / "static",
-]
+# My settings
+LOGIN_URL = '/users/login/'
+
+# Heroku settings
+if os.getcwd() == '/app':
+    import dj_database_url
+
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Only allow heroku to host the project.
+    ALLOWED_HOSTS = ['django-test.herokuapp.com']
+    DEBUG = False
+
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
-from django.http import HttpResponseRedirect
+
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -10,31 +12,39 @@ def index(request):
     return render(request, 'blog/index.html')
 
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'blog/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     topic_db = Topic.objects.get(id=topic_id)
+    if topic_db.owner != request.user:
+        raise Http404
     entries = topic_db.entry_set.order_by('-date_added')
     context = {'topic': topic_db, 'entries': entries}
     return render(request, 'blog/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
     else:
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('blog:topics'))
     context = {'form': form}
     return render(request, 'blog/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     print(topic_id)
@@ -53,9 +63,12 @@ def new_entry(request, topic_id):
     return render(request, 'blog/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
